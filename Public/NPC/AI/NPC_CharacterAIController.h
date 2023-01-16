db228d8569a3cfd812e05c0ca8480c363462fb27
GannyK <77975797+KarelGeyer@users.noreply.github.com>
2023-01-16 23:41:12 +0100

    Create a base for friendly npcs

---
+++ Public/NPC/AI/NPC_CharacterAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "NPC_CharacterAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_LARA_API ANPC_CharacterAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
---
+++ Public/NPC/Characters/NPC_Character.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NPC/NPC_Base.h"
+#include "NPC_Character.generated.h"
+
+UCLASS()
+class PROJECT_LARA_API ANPC_Character : public ANPC_Base
+{
+	GENERATED_BODY()
+
+};
